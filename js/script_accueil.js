225875c543e4e1513b3e58b98a84c4447ba5a87f
Leane BUISSON <leane.buisson@etu.univ-grenoble-alpes.fr>
2025-05-05 17:28:29 +0200

    modif

--- js/script_accueil.js
+++ js/script_accueil.js
@@ -1,42 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const searchInput = document.getElementById('search');
     const searchResultsMessage = document.getElementById('search-results-message');
-    const project = document.querySelectorAll('.project');
+    const cards = document.querySelectorAll('.card');
 
-    searchInput.addEventListener('input', function() {
+    searchInput.addEventListener('input', function () {
         const searchText = searchInput.value.toLowerCase();
         let hasVisibleCard = false;
 
-        project.forEach(card => {
-            const cardTitle = card.querySelector('h3').textContent.toLowerCase();
-            const cardDescription = card.querySelector('p').textContent.toLowerCase();
+        cards.forEach(card => {
+            const title = card.querySelector('h3').textContent.toLowerCase();
+            const description = card.querySelector('p').textContent.toLowerCase();
 
-            if (cardTitle.includes(searchText) || cardDescription.includes(searchText)) {
-                if (card.classList.contains('hidden')) {
-                    card.classList.remove('hidden');
-                    card.offsetHeight; // Trigger reflow to restart the transition
-                }
-                card.style.display = 'flex'; // Assure que la carte est affichée
+            if (title.includes(searchText) || description.includes(searchText)) {
+                card.style.display = 'flex';
                 hasVisibleCard = true;
             } else {
-                if (!card.classList.contains('hidden')) {
-                    card.style.display = 'flex'; // Assure que la carte est affichée avant la transition
-                    card.classList.add('hidden');
-                    card.addEventListener('transitionend', function handler() {
-                        card.style.display = 'none'; // Cache la carte après la fin de la transition
-                        card.removeEventListener('transitionend', handler);
-                    });
-                }
+                card.style.display = 'none';
             }
         });
 
-        // Affiche ou cache le message de résultats en fonction des résultats de la recherche
-        if (hasVisibleCard) {
-            searchResultsMessage.classList.remove('show');
-        } else {
-            searchResultsMessage.classList.add('show');
-        }
+        searchResultsMessage.style.display = hasVisibleCard ? 'none' : 'block';
     });
 });
+
 
 
